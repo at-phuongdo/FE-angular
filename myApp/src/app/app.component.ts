7fe028ec1ccc9be1afb00272f13fba3050f9c249
at-phuongdo <dophuong13t2@gmail.com>
2017-08-07 10:31:41 +0700

    Change

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { DetailComponent } from './detail/detail.component';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [appService] 
+  providers: [appService]
 })
 export class AppComponent {
 
